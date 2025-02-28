a1028475ca1ba9960bc7f01fc891e18451aae4d8
Yosri2900 <yketa057@uottawa.ca>
2025-02-27 21:54:21 -0500

    lcd display shows the current volume & mp3 is able to play songs. need to fix track names on lcd display

--- sketch_jan31a.ino
+++ sketch_jan31a.ino
@@ -1,49 +1,114 @@
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
+#include <DFRobotDFPlayerMini.h>
+#include <SoftwareSerial.h>
 const uint8_t LEDRED = 13;
 const uint8_t LEDYELLOW = 12;
-// const uint8_t LEDGREEN = 11;
+const uint8_t LEDGREEN = 11;
 const int ledPins[] = {LEDRED, LEDYELLOW};  // Add more LEDs here if needed
 const char* ledColors[] = {"RED", "YELLOW"};  // Corresponding names
 const int numLeds = sizeof(ledPins) / sizeof(ledPins[0]);
+const char* trackNames[] = {
+    "Track 1: Song A", 
+    "Track 2: Song B",
+    "Track 3: Song C" 
+};
+
 const int xPin = A0;
 int currentLEDIndex = 0; 
 int potPin = A1; // Potentiometer output connected to analog pin 3
-
+const int buttonPin = 2;
 // Set the LCD address to 0x3F for a 16 chars and 2 line display
 LiquidCrystal_I2C lcd(0x3F, 16, 2);
+// MP3 Player
+DFRobotDFPlayerMini mp3;
+SoftwareSerial mySerial(10, 11);  // RX, TX
+
+int trackCount;
+int currentTrack = 1;
+int buttonState = 0;
+unsigned long lastMoveTime = 0;  // Store the last movement time
+const int debounceDelay = 300;   // Set the debounce delay (300ms)
+int lastVolumeLevel = -1;
+
 
 void setup() {
+  Serial.begin(9600);
+  mySerial.begin(9600);
   for (uint8_t i = 0; i < numLeds; i++) {
       pinMode(ledPins[i], OUTPUT);
       digitalWrite(ledPins[i], LOW);
   }
   digitalWrite(ledPins[currentLEDIndex], HIGH);
-  Serial.begin(9600);
+  pinMode(buttonPin, INPUT);
   lcd.begin();
   lcd.backlight();
   updateLCD();
-  while (!Serial);
+  Serial.print("Number of tracks=");
+  Serial.println(trackCount);
+  if (!mp3.begin(mySerial)) {
+      Serial.println("MP3 Module Not Found!");
+      while (true);
+  }
+  Serial.println("MP3 Module Ready!");
+  mp3.volume(15);
+  delay(1000);
+
+  // Get total tracks
+  trackCount = mp3.readFileCounts();
+  Serial.print("Total Tracks on SD Card: ");
+  Serial.println(trackCount);
+  updateLCD();
 }
 
 void loop() {
   int xVal = analogRead(xPin);
-  int potVal = potVal = analogRead(potPin);
+  int potVal = analogRead(potPin);
+  // Map potentiometer value (0-1023) to volume range (0-30)
+  int volumeLevel = map(potVal, 0, 1023, 0, 30);
+  if (volumeLevel != lastVolumeLevel) {
+    mp3.volume(volumeLevel);
+    lastVolumeLevel = volumeLevel;
+    updateLCD();
+  }
 
   Serial.print("X = ");
   Serial.println(xVal);
-  Serial.println("potentiometer: ");
-  Serial.print(potVal);
+  Serial.print("Mapped Volume = ");
+  Serial.println(volumeLevel);
+
+  int totalTracks = mp3.readFileCounts();
+  Serial.print("Total Tracks: ");
+  Serial.println(totalTracks);
+
 
   // Detect right movement
-  if (xVal >= 1020) {  
+  unsigned long currentTime = millis();
+  if (xVal >= 1000 && (currentTime - lastMoveTime >= debounceDelay)) {  
       moveLED(1);  // Move right
-      delay(300);
+      currentTrack++;
+      if (currentTrack > trackCount) {
+        currentTrack = 1;
+      }
+      Serial.print("Playing previous track: ");
+      Serial.println(currentTrack);
+      mp3.play(currentTrack);
+      updateLCD();
+      lastMoveTime = currentTime;
+      mp3.play(1);
   }
   // Detect left movement
-  else if (xVal <= 10) {  
+  else if (xVal <= 300 && (currentTime - lastMoveTime >= debounceDelay)) {  
       moveLED(-1); // Move left
-      delay(300);
+        currentTrack--;
+        if (currentTrack < 1) {
+            currentTrack = trackCount;
+        }
+        Serial.print("Playing previous track: ");
+        Serial.println(currentTrack);
+        mp3.play(currentTrack);
+        updateLCD();
+        lastMoveTime = currentTime;
   }
 }
 
@@ -62,9 +127,19 @@ void moveLED(int direction) {
 
 // Function to update LCD display with current LED color
 void updateLCD() {
-    lcd.clear();
-    lcd.setCursor(0, 0);  
-    lcd.print("Current LED:");
-    lcd.setCursor(0, 1);
-    lcd.print(ledColors[currentLEDIndex]);  // Print color name
+  lcd.clear();
+  
+  // Display Track Name
+  lcd.setCursor(0, 0);
+  if (trackCount > 0 && currentTrack >= 1 && currentTrack <= trackCount) {
+    Serial.print(trackNames[currentTrack - 1]);
+    Serial.print(currentTrack);
+    
+      lcd.print(trackNames[currentTrack - 1]);
+  }
+
+  // Display Volume Level Always
+  lcd.setCursor(0, 1);
+  lcd.print("Volume: ");
+  lcd.print(lastVolumeLevel);
 }
